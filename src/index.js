639d3c5010c8e00f5d298646ccc7653f66c743ac
unknown <C:\The Bat!>
2024-09-20 16:26:21 +0300

    upd

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-const numbers = [2, 3, 5];
+import './index.css'
 
-// Стрелочная функция. Не запнётся ли на ней Internet Explorer?
-const doubledNumbers = numbers.map(number => number * 2);
 
-console.log(doubledNumbers); // 4, 6, 10 
+// const burgerMenu = document.querySelector('.burger-menu');
+
+// burgerMenu.addEventListener('click', () => {
+//     burgerMenu.classList.toggle('active');
+// });
+
