81b9af8d57cb01916017b238fcd6c95641081360
unknown <C:\The Bat!>
2024-09-24 17:04:08 +0300

    upd

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
 import './index.css'
+import './js/swiper'
+
+const burgerMenu = document.querySelector('.burger-menu');
+
+burgerMenu.addEventListener('click', () => {
+    burgerMenu.classList.toggle('active');
+});
 
 
-// const burgerMenu = document.querySelector('.burger-menu');
+// function updateSVGPath(value) {
+//     const svgPath = document.querySelector('#dynamic-svg path');
+//     const dAttribute = svgPath.getAttribute('d');
+//     const newValue = dAttribute.replace(/40(?=\s)/, value);
+//     svgPath.setAttribute('d', newValue);
+// }
 
-// burgerMenu.addEventListener('click', () => {
-//     burgerMenu.classList.toggle('active');
-// });
+// function handleResize() {
+//     const screenWidth = window.innerWidth;
+//     const newValue = Math.floor(screenWidth / 40); // Пример вычисления нового значения
+//     updateSVGPath(newValue);
+// }
+
+// window.addEventListener('resize', handleResize);
+
+// // Инициализация при загрузке страницы
+// handleResize();
 
